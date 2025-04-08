866c29318bd453d8c36cdb45f2674c5551ddc1d2
atwanzireT <tatwanzire@gmail.com>
2025-04-08 12:10:37 +0300

    fixing static

--- greatpearl_backend/settings.py
+++ greatpearl_backend/settings.py
@@ -145,7 +145,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # dev files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 # Media files (uploaded by users)
 MEDIA_URL = '/media/'
